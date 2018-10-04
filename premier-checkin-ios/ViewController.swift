86c866955db341f35538bb372a56ee299815a75d
elie808 <elie808.k@gmail.com>
2018-10-05 02:01:33 +0300

    Initial Commit

---
+++ premier-checkin-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  premier-checkin-ios
+//
+//  Created by Elie El Khoury on 10/5/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
