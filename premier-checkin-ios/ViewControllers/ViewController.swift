608a101aa277a4dec51e365e654bf4ba35f75369
elie808 <elie808.k@gmail.com>
2018-10-05 06:02:06 +0300

    Create custom Keyboard class and XIB. Integrate it in dummy viewcontroller for testing

--- premier-checkin-ios/ViewControllers/ViewController.swift
+++ premier-checkin-ios/ViewControllers/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , KeyboardDelegate {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize custom keyboard
+        let keyboardView = Keyboard(frame: CGRect(x: 0, y: 0, width: 0, height: 300))
+        keyboardView.delegate = self
+        
+        // replace system keyboard with custom keyboard
+        textField.inputView = keyboardView
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK: - KeyboardDelegate
+    
+    func keyWasTapped(character: String) {
+        textField.insertText(character)
+    }
+    
+    func backspaceTapped() {
+        textField.deleteBackward()
+    }
 }
 
---
+++ premier-checkin-ios/Views/Keyboard.swift
@@ -0,0 +1,53 @@
+//
+//  Keyboard.swift
+//  premier-checkin-ios
+//
+//  Created by Elie El Khoury on 10/5/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+protocol KeyboardDelegate: class {
+    func keyWasTapped(character: String)
+    func backspaceTapped()
+}
+
+class Keyboard: UIView {
+
+    weak var delegate: KeyboardDelegate?
+    
+    // MARK:- Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initializeSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeSubviews()
+    }
+    
+    func initializeSubviews() {
+        let xibFileName = "Keyboard" // xib extention not included
+        let view = Bundle.main.loadNibNamed(xibFileName, owner: self, options: nil)?[0] as! UIView
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    // MARK:- Actions
+    
+    @IBAction func keyTapped(sender: UIButton) {
+ 
+        if delegate != nil {
+            self.delegate?.keyWasTapped(character: sender.titleLabel!.text!) // could alternatively send a tag value
+        }
+    }
+    
+    @IBAction func didTapBackspace(sender: UIButton) {
+        if delegate != nil {
+            self.delegate?.backspaceTapped()
+        }
+    }
+}
