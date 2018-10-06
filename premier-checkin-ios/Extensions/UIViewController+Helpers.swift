358e34f227c4ce53dff24edff2c785dbf1220290
elie808 <elie808.k@gmail.com>
2018-10-06 15:17:17 +0300

    Create UIViewController helpers class

---
+++ premier-checkin-ios/Extensions/UIViewController+Helpers.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController+Helpers.swift
+//  premier-checkin-ios
+//
+//  Created by Elie El Khoury on 10/6/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func show(alert title:String, message:String, buttonTitle:String) {
+        
+        let actionSheet : UIAlertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        actionSheet.addAction(UIAlertAction(title: buttonTitle, style: UIAlertAction.Style.default, handler: { (action) in
+            
+        }))
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+}
---
+++ premier-checkin-ios/ViewControllers/LoadingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingViewController.swift
+//  premier-checkin-ios
+//
+//  Created by Elie El Khoury on 10/5/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
