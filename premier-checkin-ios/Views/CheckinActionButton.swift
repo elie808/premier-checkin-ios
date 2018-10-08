3299f58c41e8abee030569b5d23dd4774541673f
elie808 <elie808.k@gmail.com>
2018-10-08 21:15:28 +0300

    Create class for participant checkin action buttons. Add needed assets

---
+++ premier-checkin-ios/Views/CheckinActionButton.swift
@@ -0,0 +1,41 @@
+//
+//  CheckinActionButton.swift
+//  premier-checkin-ios
+//
+//  Created by Elie El Khoury on 10/8/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+class CheckinActionButton: UIButton {
+
+    // MARK: - Inspectables
+    
+    @IBInspectable public var borderWidth: CGFloat = 2.0 {
+        didSet {
+            self.layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable public var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = self.borderColor.cgColor
+        }
+    }
+    
+    // MARK: - Views Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        addRoundedCorners()
+        
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.masksToBounds = true
+    }
+
+}
