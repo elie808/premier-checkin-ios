11a2fd428235fdaf23736fd03ffb63b59858ef33
elie808 <elie808.k@gmail.com>
2018-11-09 19:50:16 +0200

    Add support to convert Checkin obj to dictionary and support RLM

--- premier-checkin-ios/Models/Checkin.swift
+++ premier-checkin-ios/Models/Checkin.swift
@@ -9,42 +9,91 @@
 import UIKit
 import RealmSwift
 
-class Checkin: Decodable {
-    var errors : [SyncObject]?
-    var updates : [SyncObject]?
+class Checkin : Codable {
+    
+//    var errors = List<SyncObject>()
+//    var updates = List<SyncObject>()
+
+    var errors : [SyncObject] = []
+    var updates : [SyncObject] = []
+
+    private enum CodingKeys: String, CodingKey {
+        case errors
+        case updates
+    }
+
+    public required convenience init(from decoder: Decoder) throws {
+        self.init()
+
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.errors = try container.decodeIfPresent([SyncObject].self, forKey: .errors)!
+        self.updates = try container.decodeIfPresent([SyncObject].self, forKey: .updates)!
+    }
 }
 
-class SyncObject: Object, Decodable {
-    
+class SyncObject: Object, Codable {
+
     @objc dynamic var sync_id = ""
     @objc dynamic var quantity = ""
     @objc dynamic var checkin_date : String?
-    
-//    @objc dynamic var checkins_pending : Int?
-    @objc dynamic var response_code : String?
-    @objc dynamic var response_message : String?
-    
+    @objc dynamic var checkins_pending : String?
+    @objc dynamic var response_code : String = ""
+    @objc dynamic var response_message : String = ""
+
+    convenience init(sync_id: String, quantity: String, checkin_date: String) {
+        self.init()
+
+        self.sync_id  = sync_id
+        self.quantity = quantity
+        self.checkin_date = checkin_date
+    }
+
     override class func primaryKey() -> String? {
         return "sync_id"
     }
-    
+
     private enum CodingKeys: String, CodingKey {
         case sync_id
         case quantity
         case checkin_date
+        case checkins_pending
         case response_code
         case response_message
     }
     
+//    func encode(to encoder: Encoder) throws {
+//
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//
+//        try container.encode(self.sync_id, forKey: .sync_id)
+//        try container.encode(self.quantity, forKey: .quantity)
+//        try container.encode(self.checkin_date, forKey: .checkin_date)
+//    }
+
+    func convertToDict() -> [String : Any] {
+        
+        let dicc = [
+                CodingKeys.sync_id.rawValue : self.sync_id,
+                CodingKeys.quantity.rawValue : self.quantity,
+                CodingKeys.checkin_date.rawValue : self.checkin_date,
+                CodingKeys.checkins_pending.rawValue : self.checkins_pending,
+                CodingKeys.response_code.rawValue : self.response_code,
+                CodingKeys.response_message.rawValue : self.response_message
+        ]
+        
+        return dicc as [String : Any]
+    }
+    
     public required convenience init(from decoder: Decoder) throws {
         self.init()
-        
+
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        
+
         self.sync_id  = try container.decode(String.self, forKey: .sync_id)
         self.quantity = try container.decode(String.self, forKey: .quantity)
         self.checkin_date = try container.decode(String.self, forKey: .checkin_date)
-        
+        self.checkins_pending  = try container.decode(String.self, forKey: .checkins_pending)
         self.response_code = try container.decode(String.self, forKey: .response_code)
         self.response_message = try container.decode(String.self, forKey: .response_message)
     }
