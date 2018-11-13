0a9021ea578fd327b118b5ccb391f932f369aed5
elie808 <elie808.k@gmail.com>
2018-11-13 02:47:41 +0200

    Create UserDefaults class and integrate throughout app

---
+++ premier-checkin-ios/Managers/UserDefaults.swift
@@ -0,0 +1,82 @@
+//
+//  UserDefaults.swift
+//  premier-checkin-ios
+//
+//  Created by Elie El Khoury on 11/13/18.
+//  Copyright © 2018 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+struct Defaults {
+    
+    // Mark: - Constants
+    
+    private static let kEventCode : String = "3796204"
+    private static let kAppSecret : String = "ZnX59SzKHgzuYuVjoE5s"
+
+    // Mark: - Keys
+    
+    private static let kEventCodeKey = "premiere_sports_event_code"
+    private static let kAppSecretKey = "premiere_sports_secret"
+    private static let kLastSyncKey  = "premiere_sports_last_sync"
+    
+    // Mark: - Save Methods
+    
+    static func saveLastSyncDate() {
+        UserDefaults.standard.set(Date(), forKey: kLastSyncKey)
+    }
+    
+    static func saveEventCode(code:String) {
+        UserDefaults.standard.set(code, forKey: kEventCodeKey)
+    }
+    
+    static func persistDefaults() {
+        let defaults = UserDefaults.standard
+        
+        defaults.set(kEventCode, forKey: kEventCodeKey)
+        defaults.set(kAppSecret, forKey: kAppSecretKey)
+    }
+    
+    // Mark: - GET Methods
+    
+    static var eventCode = { _ -> String in
+        return ((UserDefaults.standard.value(forKey: kEventCodeKey) as? String) ?? kEventCode)
+    }(())
+
+    static var appSecret = { _ -> String in
+        return ((UserDefaults.standard.value(forKey: kAppSecretKey) as? String) ?? kAppSecret)
+    }(())
+   
+    static var lastSyncDate = { _ -> String in
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd - HH:mm"
+        dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+        
+        let savedDate = (UserDefaults.standard.value(forKey: kLastSyncKey)) as? Date
+        
+        if savedDate == nil {
+            return "_"
+        } else {
+            return dateFormatter.string(from: savedDate!)
+        }
+    }(())
+    
+    // MARK: - Delete
+    
+    static func clearLastSyncDate() {
+        UserDefaults.standard.removeObject(forKey: kLastSyncKey)
+    }
+
+    static func clearEventCode() {
+        UserDefaults.standard.removeObject(forKey: kEventCodeKey)
+    }
+    
+    static func clearUserDefaults(){
+        UserDefaults.standard.removeObject(forKey: kEventCodeKey)
+        UserDefaults.standard.removeObject(forKey: kAppSecretKey)
+        UserDefaults.standard.removeObject(forKey: kLastSyncKey)
+    }
+
+}
